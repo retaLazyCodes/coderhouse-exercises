33d88dcb0416ac21778781c3ffe1fcbd50ca452f
Brian Retamar <leo1632@hotmail.com>
2022-09-12 21:29:11 -0300

    added yargs config

--- entregable12/index.js
+++ entregable12/index.js
@@ -1,6 +1,6 @@
 const { httpServer } = require('./app')
-
-const port = process.env.PORT || 8080
+const argv = require('./config/yargs.config')
+const port = argv.PORT
 
 httpServer.listen(port, () => {
   console.log(`Express server listening on: \nhttp://localhost:${port}\nhttp://localhost:${port}/api/productos\nhttp://localhost:${port}/api/productos-test`)
